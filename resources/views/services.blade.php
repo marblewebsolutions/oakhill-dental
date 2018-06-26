6fd397ed48161d593da0b1d4414402c052407c69
EC2 Default User <ec2-user@ip-172-31-36-240.us-east-2.compute.internal>
2018-06-26 23:42:01 +0000

    services title

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -18,24 +18,7 @@
                     care.</b></p>
                 </div>
             </div><div class="split-2">
-                <h3>Welcome to the Oakhill Dental Associates Website!</h3>
-                
-                <p class="mt-med">We hope you explore our site to learn about our practice. We 
-                believe that informed patients are the most successful in their 
-                journey to a healthy smile. We are here to assist you in that 
-                journey.</p>
-                
-                <p class="mt-med">Delta Dental is committed to excellence, compassion, and building 
-                lifelong relationships with our patient family. Since oral care 
-                is an important part of your overall wellness we provide a full 
-                range of dental services to help you achieve your optimal 
-                health. These services include preventive, cosmetic and 
-                restorative dentistry as well as treatment of snoring and sleep 
-                apnea with dental appliance therapy.</p>
-                
-                <p class="mt-med">We are proud that our practice focuses on not only creating 
-                beautiful smiles but also has expertise in solving complex jaw 
-                joint problems for a lifetime of beautiful and healthy smiles.</p>
+
             </div>
         </div>
         
