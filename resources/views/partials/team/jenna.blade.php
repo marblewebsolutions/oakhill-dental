bc1d8952f76f60c2f255c29472425e8a1b2ec8a6
EC2 Default User <ec2-user@ip-172-31-36-240.us-east-2.compute.internal>
2018-08-22 00:07:27 +0000

    all staff in

---
+++ resources/views/partials/team/jenna.blade.php
@@ -0,0 +1,6 @@
+@extends('partials.team.member', [
+    'name' => 'Jenna',
+    'role' => ' Dental Assistant',
+    'profile_pic' => asset('images/profiles/Jenna-glasses.JPG'),
+    'bio' => 'Jenna recently joined our team in April 2017.  Growing up in a large family, she is a big people person who provides a warm welcome to our patients.  She has resided in St Johns her entire life.   Her family is unique in that it consists of two sets of twins.   Jenna is one of the older sets.   She has 2 brothers and 2 sisters. She recently remodeled the family farm house, where she lives with her boyfriend and their golden retriever Maybel.  In her free time she enjoys spending time with family and friends, playing volleyball, softball and staying active.'
+])
--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -7,10 +7,12 @@
             <h1 class="title text-center mb-med show-on-desktop">A Lifetime of Beautiful, Healthy Smiles</h1>
             
             @include('partials.team.amber')
-            @include('partials.team.patty')
+            @include('partials.team.bianca')
             @include('partials.team.cammie')
-           
             @include('partials.team.cyndi')
+            @include('partials.team.jenna')
+            @include('partials.team.patty')
+            @include('partials.team.tina')
             
             <div class="slide-controls show-on-mobile">
                 <div class="arrow-left"></div>
