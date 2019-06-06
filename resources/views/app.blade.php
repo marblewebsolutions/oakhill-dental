bb77d7e168f51487059228882f2294941b0dfd5a
EC2 Default User <ec2-user@ip-172-31-36-240.us-east-2.compute.internal>
2019-06-06 19:53:23 +0000

    google analytics code round 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141665517-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-141665517-1');
+    </script>
+    
     <title>{{ (!empty($page_title) ? $page_title . ' | ' : '') }} Oakhill Dental Associates P.L.L.C.</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,16 +22,6 @@
 
     <!-- site css -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141665517-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'UA-141665517-1');
-    </script>
 </head>
 <body>
     <div class="view {{ strtolower($page_class) }}">
