2dab83143ad28e73ff45f3c6e8da9be3c89c5c47
EC2 Default User <ec2-user@ip-172-31-36-240.us-east-2.compute.internal>
2019-08-25 18:15:38 +0000

    mobile promotional

--- resources/views/partials/home/block-promotion.blade.php
+++ resources/views/partials/home/block-promotion.blade.php
@@ -23,7 +23,7 @@
     
     <div class="invisalign-border show-on-mobile">
         <p class="text-center"><img class="img" src="{{ asset('images/services/invisalign-88.jpg') }}" alt="Invisalign User"></p>
-        <p><span class="large-text invisalign-blue underline sub mr-sm">$500 OFF</span> PLUS Get a <span class="underline">FREE</span> 3D Invisalign Scan!</p>
+        <p class="text-center"><span class="large-text invisalign-blue underline sub mr-sm">$500 OFF</span> PLUS Get a <span class="underline">FREE</span> 3D Invisalign Scan!</p>
         <p class="mt-md"><img class="invisalign-img" src="{{ asset('images/Invisalign-Logo-blue.png') }}" alt="Invisalign Logo"></p>
         <ul>
             <li>Instant results to see your smile after Invisalign</li>
@@ -35,8 +35,8 @@
         <p class="text-center large-text mt-md">Get <span class="larger-text invisalign-blue sub mx-sm">$500</span> OFF</p>
         <p class="text-center"><img class="invisalign-img" src="{{ asset('images/Invisalign-Logo-blue.png') }}" alt="Invisalign Logo"></p>
         <p class="text-center">LIMITED TIME ONLY</p>
-        <p class="my-sm"><img class="img" src="{{ asset('images/invisalign-results-before-and-after.jpg') }}" alt="Before and After Invisalign"></p>
-        <p class="large-text">Call to schedule your <span class="underline">free consultation</span> today!</p>
-        <p class="mt-sm"><a class="large-text invisalign-blue" href="tel:1-517-484-4455">517-484-4455</a></p>
+        <p class="text-center my-sm"><img class="img" src="{{ asset('images/invisalign-results-before-and-after.jpg') }}" alt="Before and After Invisalign"></p>
+        <p class="text-center large-text">Call to schedule your <span class="underline">free consultation</span> today!</p>
+        <p class="text-center mt-sm"><a class="large-text invisalign-blue" href="tel:1-517-484-4455">517-484-4455</a></p>
     </div>
 </div>
